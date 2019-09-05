fdebccc2389ddde97848f72732b490ce8c9eb4ff
Sean McDonnell <seanmcdonnell@Seans-MacBook-Pro.local>
2019-09-05 17:49:31 -0400

    Work on user custom middleware

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code away!
+
+const server = require('./server.js');
+
+server.listen(8000, () => {
+    console.log('Listening on port 8000');
+});
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,100 @@
-const express = 'express';
+const express = require('express');
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+const db = require('./userDb.js');
 
+router.post('/', validateUser, (req, res) => {
+    const newUser = req.body;
+
+    db.insert(newUser)
+        .then(user => {
+            res.status(201).json(user);
+        })
+        .catch(error => {
+            res.status(500).json({ error: error, message: 'There was a server error while attempting to add user' });
+        });
 });
 
-router.post('/:id/posts', (req, res) => {
-
-});
+router.post('/:id/posts', (req, res) => {});
 
 router.get('/', (req, res) => {
-
+    db.get()
+        .then(users => {
+            res.status(200).json(users);
+        })
+        .catch(error => {
+            res.status(500).json({ error: error, message: 'There was a server error while attempting to retrieve users' });
+        });
 });
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', validateUserId, (req, res) => {
+    res.status(200).json(req.user);
 });
 
-router.get('/:id/posts', (req, res) => {
-
+router.get('/:id/posts', validateUserId, (req, res) => {
+    db.getUserPosts(req.user.id)
+        .then(posts => {
+            res.status(200).json(posts);
+        })
+        .catch(error => {
+            res.status(500).json({ error: error, message: 'There was a server error while attempting to retrieve users' });
+        });
 });
 
-router.delete('/:id', (req, res) => {
-
+router.delete('/:id', validateUserId, (req, res) => {
+    db.remove(req.user.id)
+        .then(user => {
+            res.status(200).json(req.user);
+        })
+        .catch(error => {
+            res.status(404).json({ error: error, message: 'User could not be deleted' });
+        });
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', validateUserId, validateUser, (req, res) => {
+    const newUser = req.body;
 
+    db.update(req.user.id, newUser)
+        .then(user => {
+            res.status(201).json(user);
+        })
+        .catch(error => {
+            res.status(500).json({ error: error, message: 'There was a server error while attempting to edit user' });
+        });
 });
 
 //custom middleware
 
 function validateUserId(req, res, next) {
+    const id = req.params.id;
 
-};
+    db.getById(id)
+        .then(user => {
+            if (user) {
+                req.user = user;
+                next();
+            } else {
+                res.status(404).json({ message: 'The user ID is invalid' });
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ error: error, message: 'There was a server error while attempting to retrieve users' });
+        });
+}
 
 function validateUser(req, res, next) {
+    if (Object.keys(req.body).length === 0 && req.body.constructor === Object) {
+        res.status(400).json({ message: 'missing user data' });
+    }
 
-};
+    if (req.body.name) {
+        next();
+    } else {
+        res.status(400).json({ message: 'missing required name field' });
+    }
+}
 
-function validatePost(req, res, next) {
-
-};
+function validatePost(req, res, next) {}
 
 module.exports = router;
